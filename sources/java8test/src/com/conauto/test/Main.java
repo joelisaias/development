89cd97a4a4a41ecc615f61fa8ddc3ff26a9adb4e
joelisaias <joel.isaias_at@hotmail.com>
2015-02-22 20:52:36 -0500

    Update Main.java

--- sources/java8test/src/com/conauto/test/Main.java
+++ sources/java8test/src/com/conauto/test/Main.java
@@ -82,6 +82,9 @@ public class Main {
 		        (from) -> String.valueOf(from + num);
 
 		System.out.println(stringConverter.convert(2));     // 3
+		
+		
+		System.out.println("ACTUALIZADO");
 
 		
 	}
