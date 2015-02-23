f8cc1d3a5c5bf1572009ceda9ac6fed5a98e0a22
Joel Alvarado <jalvarado@conauto.com>
2015-02-22 23:30:13 -0500

    j8

--- sources/java8test/src/com/conauto/test/Main.java
+++ sources/java8test/src/com/conauto/test/Main.java
@@ -1,7 +1,18 @@
 package com.conauto.test;
 
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoField;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
 	public static void main(String[] args) {
@@ -199,7 +210,168 @@ public class Main {
 //
 //			System.out.println(startsWithB);    // 3
 
+//		Optional<String> reduced =
+//			    stringCollection
+//			        .stream()
+//			        .sorted()
+//			        .reduce((s1, s2) -> s1 + "#" + s2);
+//
+//			reduced.ifPresent(System.out::println);
 		
+//		int max = 1000000;
+//		List<String> values = new ArrayList<>(max);
+//		for (int i = 0; i < max; i++) {
+//		    UUID uuid = UUID.randomUUID();
+//		    values.add(uuid.toString());
+//		
+//
+//		}
+		
+		
+//		long t0 = System.nanoTime();
+//
+//		long count = values.stream().sorted().count();
+//		System.out.println(count);
+//
+//		long t1 = System.nanoTime();
+//
+//		long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+//		System.out.println(String.format("sequential sort took: %d ms", millis));
+		
+		
+		
+//		long t0 = System.nanoTime();
+//
+//		long count = values.parallelStream().sorted().count();
+//		System.out.println(count);
+//
+//		long t1 = System.nanoTime();
+//
+//		long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+//		System.out.println(String.format("parallel sort took: %d ms", millis));
+
+		
+		
+//		
+//		Map<Integer, String> map = new HashMap<>();
+//
+//		for (int i = 0; i < 10; i++) {
+//		    map.putIfAbsent(i, "val" + i);
+//		}
+//
+//		map.forEach((key, val) -> System.out.println(key+" - "+val));
+//		
+//		
+//		map.computeIfPresent(3, (num, val) -> val + num);
+//		map.get(3);             // val33
+//
+//		map.computeIfPresent(9, (num, val) -> null);
+//		map.containsKey(9);     // false
+//
+//		map.computeIfAbsent(23, num -> "val" + num);
+//		map.containsKey(23);    // true
+//
+//		map.computeIfAbsent(3, num -> "bam");
+//		map.get(3);             // val33
+
+		
+		
+		//DATE API
+		
+//		Clock clock = Clock.systemDefaultZone();
+//		long millis = clock.millis();
+//
+//		System.out.println(millis);
+//		Instant instant = clock.instant();
+//		Date legacyDate = Date.from(instant);   // legacy java.util.Date
+//
+//		System.out.println(legacyDate);
+//		
+//		System.out.println(ZoneId.getAvailableZoneIds());
+//		// prints all available timezone ids
+//
+//		ZoneId zone1 = ZoneId.of("Europe/Berlin");
+//		ZoneId zone2 = ZoneId.of("Brazil/East");
+//		System.out.println(zone1.getRules());
+//		System.out.println(zone2.getRules());
+//
+//
+//		LocalTime now1 = LocalTime.now(zone1);
+//		LocalTime now2 = LocalTime.now(zone2);
+//
+//		System.out.println(now1.isBefore(now2));  // false
+//
+//		long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
+//		long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
+//
+//		System.out.println(hoursBetween);       // -3
+//		System.out.println(minutesBetween);     // -239
+
+		
+//		LocalTime late = LocalTime.of(23, 59, 59);
+//		System.out.println(late);       // 23:59:59
+//
+//		DateTimeFormatter germanFormatter =
+//		    DateTimeFormatter
+//		        .ofLocalizedTime(FormatStyle.SHORT)
+//		        .withLocale(Locale.GERMAN);
+//
+//		LocalTime leetTime = LocalTime.parse("13:37", germanFormatter);
+//		System.out.println(leetTime);   // 13:37
+//
+//		
+//		
+//		
+//		LocalDate today = LocalDate.now();
+//		LocalDate tomorrow = today.plus(1, ChronoUnit.DAYS);
+//		LocalDate yesterday = tomorrow.minusDays(2);
+//
+//		LocalDate independenceDay = LocalDate.of(2014, Month.JULY, 4);
+//		DayOfWeek dayOfWeek = independenceDay.getDayOfWeek();
+//		System.out.println(dayOfWeek);    // FRIDAY
+		
+		
+//		DateTimeFormatter germanFormatter =
+//			    DateTimeFormatter
+//			        .ofLocalizedDate(FormatStyle.MEDIUM)
+//			        .withLocale(Locale.GERMAN);
+//
+//			LocalDate xmas = LocalDate.parse("24.12.2014", germanFormatter);
+//			System.out.println(xmas);   // 2014-12-24
+//			
+//			LocalDateTime sylvester = LocalDateTime.of(2014, Month.DECEMBER, 31, 23, 59, 59);
+//
+//			DayOfWeek dayOfWeek = sylvester.getDayOfWeek();
+//			System.out.println(dayOfWeek);      // WEDNESDAY
+//
+//			Month month = sylvester.getMonth();
+//			System.out.println(month);          // DECEMBER
+//
+//			long minuteOfDay = sylvester.getLong(ChronoField.MINUTE_OF_DAY);
+//			System.out.println(minuteOfDay);    // 1439
+//
+//			
+//			
+//			Instant instant = sylvester
+//			        .atZone(ZoneId.systemDefault())
+//			        .toInstant();
+//
+//			Date legacyDate = Date.from(instant);
+//			System.out.println(legacyDate);     // Wed Dec 31 23:59:59 CET 2014
+//			
+//			
+//			DateTimeFormatter formatter =
+//				    DateTimeFormatter
+//				        .ofPattern("MMM dd, yyyy - HH:mm");
+//
+//				LocalDateTime parsed = LocalDateTime.parse("Nov 03, 2014 - 07:13", formatter);
+//				String string = formatter.format(parsed);
+//				System.out.println(string);     // Nov 03, 2014 - 07:13
+
+
+
+
+
 	}
 	
 	
